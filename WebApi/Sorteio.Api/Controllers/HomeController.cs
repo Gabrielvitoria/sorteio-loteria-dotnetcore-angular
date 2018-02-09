1dc1e47b6d585f63873a195a117b532873a54fb4
gabriel.vitoria <gabriel.vitoria.ti@gmail.com>
2018-02-09 00:11:43 -0200

    Add rota e controller par a API home

--- WebApi/Sorteio.Api/Controllers/HomeController.cs
+++ WebApi/Sorteio.Api/Controllers/HomeController.cs
@@ -5,7 +5,15 @@ namespace Sorteio.Api.Controllers
 {
     public class HomeController : Controller
     {
-
+        [HttpGet]
+        [Route("Home")]
+        // [Route("clientes")] // Listar todos os clientes
+        // [Route("clientes/2587")] // Listar o cliente 2587
+        // [Route("clientes/2587/pedidos")] // Pedidos do cliente 2587
+        public object Get()
+        {
+            return new { version = "Version 0.0.1" };
+        }
     }
 
 }
